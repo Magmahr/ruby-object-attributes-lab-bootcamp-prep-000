51aca802377f102ebac96dfab7f1a26be4cb0ea6
Matt Maher <mattmaher0@gmail.com>
2018-05-08 13:57:32 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  dog = Dog.new(name)
+  dog
+  
+  
+  def name
+    @name
+  end  
+  
+  def name=(new_name)
+    @name = new_name
+  end  
+  
+  def assign(breed)
+    @breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end  
+
+end  
+    
